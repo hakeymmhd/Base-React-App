b9992bafe7d0ad5e2bfd5d4e671b6055bddfd615
mhdhakeym <mhdhakeym786@gmail.com>
2021-10-11 07:24:29 +0800

    rendered thumbnails

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import "./styles.css";
+import ImageData from "./imageData";
 
-export default function App() {
+export default function App(img, city) {
+  const arr = ImageData.map((record, index) => {
+    return (
+    <>
+    <img
+    src={record.img}
+    className="thumb"
+    alt={record.city}
+    />
+    </>)
+  });
+
   return (
     <div className="App">
-      <h1>My WeatherIcons App</h1> 
+      <h1>Cities of The World</h1> 
+        <div id="wrapper">
+          <div id="thumbnails">
+             {/* RENDER THE IMAGES ARRAY  */}
+          {arr}
+          </div>
+          {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
+        {/* <img src="" id="bigimage" alt='bigImage'/> */}
+        </div>
+     
     </div>
   );
 }

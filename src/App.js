a8cfbb1c2deec9cc53d5fcfa76fcbf71e523dc80
mhdhakeym <mhdhakeym786@gmail.com>
2021-10-11 08:54:28 +0800

    completed but need to refine useState

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import "./styles.css";
 import ImageData from "./imageData";
 
 export default function App(img, city) {
+  const initial = {
+    img: ImageData[0].img,
+    city: ImageData[0].city,
+  }
+  const [imageState, setImageState] = useState(initial);
+  console.log(imageState.city)
   const arr = ImageData.map((record, index) => {
     return (
     <>
-    <img
+    <img onClick = {() => setImageState({img: record.img, city: record.city})}
     src={record.img}
     className="thumb"
     alt={record.city}
@@ -14,7 +20,18 @@ export default function App(img, city) {
     </>)
   });
 
+
+  console.log(imageState.city)
+  const bigImage =
+      <img 
+      src={imageState.img}
+      id="bigimage"
+      alt={imageState.city}
+      />
+
+
   return (
+
     <div className="App">
       <h1>Cities of The World</h1> 
         <div id="wrapper">
@@ -24,6 +41,7 @@ export default function App(img, city) {
           </div>
           {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
         {/* <img src="" id="bigimage" alt='bigImage'/> */}
+        {bigImage}
         </div>
      
     </div>

692569cd77fedad89d0a545e7f36f947a2ff85bf
mhdhakeym <mhdhakeym786@gmail.com>
2021-10-20 22:42:54 +0800

    completed lab

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 // import useState
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './styles.css';
 import AllTheThings from './components/AllTheThings/index';
 import MyShoppingCart from './components/MyShoppingCart/index';
-import Form from './components/Form/index';
+// import Form from './components/Form/index';
 import productsArr from './products';
 
 export default function App() {
   const [products, setProducts] = useState(productsArr);
   const [cart, setCart] = useState([]);
+
+  const itemName = useRef();
+  const itemPrice = useRef();
+  const itemDesc = useRef();
 
   // create an addToCart function that takes in a product as a param
   // using the ...spread operator add the product to the cart array
@@ -24,16 +28,47 @@ export default function App() {
         setCart(cartArr)
     };
 
+    const handleSubmit = () => {
+      // console.log("clicked!");
+      // console.log(itemPrice.current.value)
+        setProducts([
+          {
+            name: `${itemName.current.value}`,
+            price: itemPrice.current.value,
+            description: `${itemDesc.current.value}`
+        }, ...products])}
 
+    // console.log("app log: " + products);
   return (
     <div className="App">
-      <h1>Big Time Shopping</h1>
-      {/* <Form products={products} handleClick={addToCart}/> */}
-      <Form />
-      <div className="products">
-        <AllTheThings products={products} handleClick={addToCart}/>
-        <MyShoppingCart cart={cart} handleClick={removeFromCart} />
+      <h1>Big Time Shopping</h1>  
+      <div className="wrapper">
+        <div className="form-signin">
+        <h2 class="form-signin-heading">Form goes here</h2>
+          <input type="text" ref={itemName} placeholder="product name" />
+          <input type="text" ref={itemPrice} placeholder="price" />
+          <input type="text" ref={itemDesc} placeholder="description" />
+          <button className="btn btn-lg btn-primary btn-block" onClick={handleSubmit}>Submit</button>
+        </div>
+        
       </div>
+    
+      <div className="products">
+        <div className="AllTheThings">
+          <AllTheThings products={products} handleClick={addToCart}/>
+        </div>
+        <div className="MyShoppingCart">
+          <MyShoppingCart cart={cart} handleClick={removeFromCart} />
+        </div>      
+      </div>
+
+
+      {/* <Form products={products} handleClick={addToCart}/> */}
+      {/* <Form /> */}
+      {/* <div className="products"> */}
+        {/* <AllTheThings products={products} handleClick={addToCart}/> */}
+        {/* <MyShoppingCart cart={cart} handleClick={removeFromCart} /> */}
+      {/* </div> */}
     </div>
   );
 }
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,27 +1,57 @@
 import React, {useState, useRef} from "react";
+import AllTheThings from '../AllTheThings/index.jsx';
+import productsArr from '../../products';
+import MyShoppingCart from '../MyShoppingCart/index';
 
-function Form() {
-  const [items, setItems] = useState({name: '', price: 0});
+function Form(props) {
+  const [products, setProducts] = useState(productsArr);
+  const [cart, setCart] = useState([]);
+  
   const itemName = useRef();
   const itemPrice = useRef();
 
-  console.log(items);
+
+  const addToCart = item => {
+    setCart( [...cart, item] )
+  };
+
+  const removeFromCart = index => {
+    const cartArr = cart.filter( (d,i) => i !== index )
+    setCart(cartArr)
+  };
+
+  console.log(products);
   const handleSubmit = () => {
+    // props.handleClick(products);
     console.log("clicked!");
     console.log(itemPrice.current.value)
-      setItems({...items,
-        name: `${itemName.current.value}`,
-        price: itemPrice.current.value,
-      })
+      setProducts([
+        {
+          name: `${itemName.current.value}`,
+          price: itemPrice.current.value,
+          description: "test"
+      }, ...products])
       
   }
  
   return (
-    <div className="Form">
-      <h2>Form goes here</h2>
-      <input type="text" ref={itemName} placeholder="product name" />
-      <input type="text" ref={itemPrice} placeholder="price" />
-      <input type="submit" onClick={handleSubmit} />
+    <div>
+      <div className="wrapper">
+        <h2>Form goes here</h2>
+        <input type="text" ref={itemName} placeholder="product name" />
+        <input type="text" ref={itemPrice} placeholder="price" />
+        <input type="submit" onClick={handleSubmit} />
+      </div>
+    
+      
+      <div className="products">
+        <div className="AllTheThings">
+          <AllTheThings products={products} handleClick={addToCart}/>
+        </div>
+        <div className="MyShoppingCart">
+          <MyShoppingCart cart={cart} handleClick={removeFromCart} />
+        </div>      
+      </div>
     </div>
   );
 }

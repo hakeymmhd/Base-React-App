1126e1b85e52fa7a1c4ff991245524951cf4c73b
mhdhakeym <mhdhakeym786@gmail.com>
2021-10-23 20:28:22 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from './components/App'
+import {BrowserRouter as Router} from "react-router-dom"
 
-import App from "./App";
+import "./styles.css";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>, 
   rootElement
-);
+  );
+
